5255e6bd412a6814d23a3a574f6cf5bf0b6eacb7
Abdullah al Bake <abdullah.bake@leads-bd.com>
2019-06-26 13:50:22 +0600

    Welcome Splash Screen

---
+++ app/src/main/java/com/example/need/HomeActivity.java
@@ -0,0 +1,32 @@
+package com.example.need;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Toast;
+
+public class HomeActivity extends AppCompatActivity {
+    private long backPressedTime;
+    private Toast backToast;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+    }
+
+    @Override
+    public void onBackPressed() {
+
+        if (backPressedTime+2000>System.currentTimeMillis())
+        {
+            backToast.cancel();
+            super.onBackPressed();
+        }
+        else
+        {
+            backToast=Toast.makeText(getBaseContext(), "Press back again to exit", Toast.LENGTH_SHORT);
+            backToast.show();
+        }
+        backPressedTime=System.currentTimeMillis();
+    }
+}
